bb7ee22058637456e2fb4808eb8783037aec4a20
Ch4nYH <hfercxx@hotmail.com>
2021-06-23 11:40:27 +0800

    updaate

--- main.py
+++ main.py
@@ -54,7 +54,11 @@ class ImageFolderTwoTransform(datasets.ImageFolder):
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return np.array(sample), sample1, target
+        return transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor()
+    ])(sample), sample1, target
 
 traindir = os.path.join(sys.argv[1], 'train')
 valdir = os.path.join(sys.argv[1], 'val')
