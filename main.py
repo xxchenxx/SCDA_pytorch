afc93fe84a36ac84557f83d3bc150100bc124ec6
Ch4nYH <hfercxx@hotmail.com>
2021-06-23 11:33:11 +0800

    update

--- main.py
+++ main.py
@@ -100,7 +100,7 @@ for ii, (img, label) in enumerate(train_loader):
     import matplotlib.pyplot as plt
     for i in range(m):
         plt.figure()
-        plt.imshow(m[i])
+        plt.imshow(img[i])
         plt.imshow(f_re[i].mean(0), alpha=0.2, aspect="auto")
         plt.axis("off")
         plt.savefig(f"LTH/{sys.argv[2]}/{i}.png")
